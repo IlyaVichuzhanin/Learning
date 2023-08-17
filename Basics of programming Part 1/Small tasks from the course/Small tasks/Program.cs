82e7411e55474ff9dedbd94d7d3a0b0705ea96d1
Вычужанин Илья Андреевич <Vychuzhanin.IA@pnsh.ru>
2023-08-17 14:11:51 +0500

    17/08/2023

---
+++ Basics of programming Part 1/Small tasks from the course/Small tasks/Program.cs
@@ -0,0 +1,10 @@
+﻿using Small_tasks.Chapter_1;
+
+public class Programm
+{
+    public static void Main()
+    {
+        //Bitcoins.CountBitcoins();
+        FunctionMinimum.WriteRoots();
+    }
+}
